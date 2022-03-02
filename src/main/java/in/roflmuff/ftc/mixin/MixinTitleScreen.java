87470b94baa3e6bb5fac56d14af271881c50824b
Madis <Madis0@users.noreply.github.com>
2022-03-02 20:47:44 +0200

    Some metadata

--- src/main/java/in/roflmuff/ftc/mixin/MixinTitleScreen.java
+++ src/main/java/in/roflmuff/ftc/mixin/MixinTitleScreen.java
@@ -21,8 +21,8 @@ public abstract class MixinTitleScreen extends Screen {
 
     @Shadow @Final private boolean doBackgroundFade;
     @Shadow private long backgroundFadeStart;
-    private Text puzzleText;
-    private int puzzleTextWidth;
+    private Text menuText;
+    private int menuTextWidth;
     private int yOffset = 20;
 
     protected MixinTitleScreen(Text title) {
@@ -30,8 +30,8 @@ public abstract class MixinTitleScreen extends Screen {
     }
     @Inject(at = @At("TAIL"), method = "init")
     private void puzzle$init(CallbackInfo ci) {
-        puzzleText = Text.of(FabricTitleChangerConfig.text);
-        this.puzzleTextWidth = this.textRenderer.getWidth(puzzleText);
+        menuText = Text.of(FabricTitleChangerConfig.text);
+        this.menuTextWidth = this.textRenderer.getWidth(menuText);
     }
 
     @Inject(at = @At("TAIL"), method = "render")
@@ -39,9 +39,9 @@ public abstract class MixinTitleScreen extends Screen {
         float f = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
         float g = this.doBackgroundFade ? MathHelper.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
         int l = MathHelper.ceil(g * 255.0F) << 24;
-        textRenderer.drawWithShadow(matrices, puzzleText,2,this.height - yOffset, 16777215 | l);
-        if (mouseX > 2 && mouseX < 2 + this.puzzleTextWidth && mouseY > this.height - yOffset && mouseY < this.height - yOffset + 10) {
-            fill(matrices, 2, this.height - yOffset + 9, 2 + this.puzzleTextWidth, this.height - yOffset + 10, 16777215 | l);
+        textRenderer.drawWithShadow(matrices, menuText,2,this.height - yOffset, 16777215 | l);
+        if (mouseX > 2 && mouseX < 2 + this.menuTextWidth && mouseY > this.height - yOffset && mouseY < this.height - yOffset + 10) {
+            fill(matrices, 2, this.height - yOffset + 9, 2 + this.menuTextWidth, this.height - yOffset + 10, 16777215 | l);
         }
     }
 
@@ -54,7 +54,7 @@ public abstract class MixinTitleScreen extends Screen {
 
     @Inject(at = @At("HEAD"), method = "mouseClicked",cancellable = true)
     private void puzzle$mouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
-        if (mouseX > 2 && mouseX < (double)(2 + this.puzzleTextWidth) && mouseY > (double)(this.height - yOffset) && mouseY < (double)this.height - yOffset + 10) {
+        if (mouseX > 2 && mouseX < (double)(2 + this.menuTextWidth) && mouseY > (double)(this.height - yOffset) && mouseY < (double)this.height - yOffset + 10) {
             if (Objects.requireNonNull(this.client).options.chatLinksPrompt) {
                 this.client.setScreen(new ConfirmChatLinkScreen(this::confirmLink, FabricTitleChangerConfig.url, true));
             } else {
